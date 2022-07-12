2378382f194a31ad4938b7b72614c513ae6af1f1
evelyntys <evelyn.tan@trentglobal.edu.sg>
2022-07-12 08:14:38 +0000

    added ddl commands

---
+++ ddl.sql
@@ -0,0 +1,15 @@
+# Data definition language
+$ need to have ; behind
+
+# create a database
+create database employees;
+
+# show databases
+show databases; 
+
+# before we can issue commands for a database
+# we must set the current active database
+use employees;
+
+# tells you what database you are currently using
+select database();
